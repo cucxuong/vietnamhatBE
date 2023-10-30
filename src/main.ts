fe2e564823084a129ccbcfcc208d3d1d1b20b729
nhannv-zigexn <nhannv@zigexn.vn>
2023-10-31 00:07:55 +0700

    Auth API OK

--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,19 @@
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import * as cookieParser from 'cookie-parser';
+import {ConfigService} from "@nestjs/config";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const configService = app.get(ConfigService);
 
   // app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
-  app.enableCors();
+  app.enableCors({
+    origin: [configService.get<string>('FE_ORIGIN') ?? '*'],
+    credentials: true,
+  });
+  app.use(cookieParser());
 
   useContainer(app.select(AppModule), { fallbackOnErrors: true });
 
