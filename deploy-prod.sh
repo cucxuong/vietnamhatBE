6f6a03334d751f3a1a04a3cee72510c305e88169
root <root@sv.nvnhan0810.com>
2023-10-31 18:01:26 +0700

    Update deploy script

--- deploy-prod.sh
+++ deploy-prod.sh
@@ -1,10 +1,26 @@
 #!/bin/sh
 
 DATE=`date +%Y.%m.%d.%H.%M`
-CONTAINER_NAME=vietnam-ultimate-be-container
-IMAGE_NAME=vietnam-ultimate-be
+CONTAINER_NAME=stg-vietnam-ultimate-be-container
+IMAGE_NAME=stg-vietnam-ultimate-be
+NETWORK_NAME=stg-vietnam-ultimate-be-net
+EXPOSE_PORT=3002
+BRANCH_NAME=$(git symbolic-ref --short -q HEAD)
 
-git pull origin main
+if [[ $BRANCH_NAME == 'main' ]]; then
+    CONTAINER_NAME=vietnam-ultimate-be-container
+    IMAGE_NAME=vietnam-ultimate-be
+    NETWORK_NAME=vietnam-ultimate-be-net
+    EXPOSE_PORT=3000
+fi
+
+echo $BRANCH_NAME:
+echo $NETWORK_NAME;
+echo $IMAGE_NAME;
+echo $CONTAINER_NAME:
+echo $EXPOSE_PORT:
+
+git pull origin $BRANCH_NAME
 
 docker build -t $IMAGE_NAME:$DATE .
 
@@ -15,6 +31,6 @@ if [[ $? -eq 0 ]]; then
     docker container rm -f $CONTAINER_NAME
 fi
 
-docker run -itd -p 3000:3000 --name $CONTAINER_NAME --network nvnhan-network --network-alias vietnam-ultimate-be-net $IMAGE_NAME:$DATE
+docker run -itd -p 127.0.0.1:$EXPOSE_PORT:3000 --name $CONTAINER_NAME --network nvnhan-network --network-alias $NETWORK_NAME $IMAGE_NAME:$DATE
 
 docker system prune -a -f
